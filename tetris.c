6ee2a58492730843598fe7cb2de4f399e8442bc0
alabig <alabig@purdue.edu>
2016-04-14 18:29:48 -0400

    Update tetris.c

--- tetris.c
+++ tetris.c
@@ -1,11 +1,14 @@
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
 
+#define NUMROWS 23  // 3 extra rows to allow movement as piece appears
+#define NUMCOLS 10
+
 char x, y, size;
 char row, col;
 char ** currentpiece;
 
-char gameboard[20][10];
+char gameboard[NUMROWS][NUMCOLS];
 
 char iblock[4][4][4] = {
 {  
@@ -174,8 +177,8 @@ char oblock[4][3][3] = {
 
 void initboard(void)
 {
-  for (y = 0;y < 20;y++)
-    for (x = 0;x < 10;x++) gameboard[y][x] = 0;
+  for (y = 0;y < NUMROWS;y++)
+    for (x = 0;x < NUMCOLS;x++) gameboard[y][x] = 0;
   
 }
 
