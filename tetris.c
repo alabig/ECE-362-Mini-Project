e2d437ba8761473aaf1e50e19857514dd99f1239
alabig <alabig@purdue.edu>
2016-04-14 19:43:12 -0400

    Update tetris.c

--- tetris.c
+++ tetris.c
@@ -1,5 +1,8 @@
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
+#include <stdlib.h>
+
+#define TETRIS 4
 
 #define NUMROWS 23  // 3 extra rows to allow movement as piece appears
 #define NUMCOLS 10
@@ -7,6 +10,9 @@
 char x, y, size;
 char row, col;
 char ** currentpiece;
+
+char tick, timcnt, level;
+char roundover, gameover;
 
 char gameboard[NUMROWS][NUMCOLS];
 
@@ -175,10 +181,26 @@ char oblock[4][3][3] = {
 }
 };
 
-void initboard(void)
+void initializations(void)
 {
+  
+  /* Initialize TIM Ch 7 (TC7) for periodic interrupts every 100 ms */
+  TSCR1 = 0x80; // enable timer subsystem
+  TIOS  = 0x80; // set channel 7 for output compare
+  TSCR2 = 0x0C; // set appropriate pre-scale factor and enable counter reset after OC7
+  TC7   = 15000;// set up channel 7 to generate 100 ms interrupt rate
+  TIE   = 0x80; // enable TIM Ch 7 interrupts
+  
+  //initialize gameboard
   for (y = 0;y < NUMROWS;y++)
     for (x = 0;x < NUMCOLS;x++) gameboard[y][x] = 0;
+    
+  // other initializations
+  roundover = 1;
+  gameover = 0;
+  level = 0;
+  tick = 0;
+  timcnt = 0;
   
 }
 
@@ -190,7 +212,7 @@ int checkcollision(void)
   {
     for (x = 0; x < size; x++)
     {
-      if (currentpiece[y][x])
+      if (currentpiece[size - 1 - y][x])
         if ((col + x < 0) || (col + x > 9) || (row + y < 0) || (gameboard[row + y][col + x])) collision = 1;
     }
   }
@@ -198,13 +220,63 @@ int checkcollision(void)
   return collision;
 }
 
+void updatedisp(void)
+{
+  // update game board and display
+}
+
 void main(void) {
   /* put your own code here */
-  
-  	EnableInterrupts;
+ 	EnableInterrupts;
 
   for(;;) {
+    
+    if (tick)
+    {
+      tick = 0;
+      
+      if (roundover)
+      {
+        roundover = 0;
+        
+        // set new piece as currentpiece
+        // reset row and col for new piece
+        row = NUMROWS - 4;
+        col = NUMCOLS - 7;
+        
+        if (checkcollision()) gameover = 1;  // unable to initially place new piece, game over
+        else updatedisp();  // update board with initial placement of new piece
+      }
+      else
+      {
+        row--;
+        
+        if (checkcollision())
+        {
+          row++;
+          roundover = 1;
+        }
+      
+        updatedisp();
+      }
+    }
+  
     _FEED_COP(); /* feeds the dog */
-  } /* loop forever */
+  }/* loop forever */
   /* please make sure that you never leave main */
+}
+
+/*
+***********************************************************************
+  TIM interrupt service routine
+***********************************************************************
+*/
+
+interrupt 15 void TIM_ISR(void)
+{
+ 	TFLG1 = TFLG1 | 0x80; // clear TIM CH 7 interrupt flag 
+ 	
+ 	timcnt++;
+ 	if (timcnt == (10 - level)) tick = 1; 	   	  
+ 	
 }
