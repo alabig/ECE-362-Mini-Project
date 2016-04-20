ec0a799253b31753258b63eb01f3563edd2610d8
alabig <alabig@purdue.edu>
2016-04-20 18:15:14 -0400

    Update nes_controller.c

--- nes_controller.c
+++ nes_controller.c
@@ -1,445 +1,114 @@
-#include <hidef.h>      /* common defines and macros */
+// ***********************************************************************
+//  ECE 362 - Final Project - Spring 2016
+// ***********************************************************************
+
+#include <hidef.h>           /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
-#include <stdlib.h>
+#include <mc9s12c32.h>
 
-#define TETRIS 4
+char a = 1;
+char b = 1;
+char sel = 1;
+char start = 1;
+char up = 1;
+char down = 1;
+char left = 1;
+char right = 1;
+	 	   		
+// Initializations
+ 
+void  initializations(void) {
 
-#define NUMROWS 23  // 3 extra rows to allow movement as piece appears
-#define NUMCOLS 10
+// Set the PLL speed (bus clock = 24 MHz)
 
-#define NUMPIECES 7
-
-char x, y, size, rot;
-char row, col;
-char emptyrow;
-char *** block;
-char ** currentpiece;
-
-char tick, timcnt, level;
-char roundover, gameover;
-
-char up, down, left, right, a, b;
-
-char n, i;
-
-char rclear[4] = {0,0,0,0};  // rows to clear
-
-char gameboard[NUMROWS][NUMCOLS];
-
-int score, highscore;
-
-char iblock[4][4][4] = {
-{  
-  {0,0,0,0},
-  {0,0,0,0},
-  {1,1,1,1},
-  {0,0,0,0}
-},
-{
-  {0,1,0,0},
-  {0,1,0,0},
-  {0,1,0,0},
-  {0,1,0,0}
-},
-{
-  {0,0,0,0},
-  {1,1,1,1},
-  {0,0,0,0},
-  {0,0,0,0}
-},
-{
-  {0,0,1,0},
-  {0,0,1,0},
-  {0,0,1,0},
-  {0,0,1,0}
-}
-};
-
-char tblock[4][3][3] = {
-{  
-  {0,2,0},
-  {2,2,2},
-  {0,0,0}
-},
-{
-  {0,2,0},
-  {0,2,2},
-  {0,2,0}
-},
-{
-  {0,0,0},
-  {2,2,2},
-  {0,2,0}
-},
-{
-  {0,2,0},
-  {2,2,0},
-  {0,2,0}
-}
-};
-
-char zblock[4][3][3] = {
-{  
-  {3,3,0},
-  {0,3,3},
-  {0,0,0}
-},
-{
-  {0,3,0},
-  {3,3,0},
-  {3,0,0}
-},
-{
-  {0,0,0},
-  {3,3,0},
-  {0,3,3}
-},
-{
-  {0,0,3},
-  {0,3,3},
-  {0,3,0}
-}
-};
-
-char sblock[4][3][3] = {
-{  
-  {0,4,4},
-  {4,4,0},
-  {0,0,0}
-},
-{
-  {0,4,0},
-  {0,4,4},
-  {0,0,4}
-},
-{
-  {0,0,0},
-  {0,4,4},
-  {4,4,0},
-},
-{
-  {4,0,0},
-  {4,4,0},
-  {0,4,0}
-}
-};
-
-char jblock[4][3][3] = {
-{  
-  {5,0,0},
-  {5,5,5},
-  {0,0,0}
-},
-{
-  {0,5,5},
-  {0,5,0},
-  {0,5,0}
-},
-{
-  {0,0,0},
-  {5,5,5},
-  {0,0,5}
-},
-{
-  {0,5,0},
-  {0,5,0},
-  {5,5,0}
-}
-};
-
-char lblock[4][3][3] = {
-{  
-  {0,0,6},
-  {6,6,6},
-  {0,0,0}
-},
-{
-  {0,6,0},
-  {0,6,0},
-  {0,6,6}
-},
-{
-  {0,0,0},
-  {6,6,6},
-  {6,0,0}
-},
-{
-  {6,6,0},
-  {0,6,0},
-  {0,6,0}
-}
-};
-
-char oblock[4][3][3] = {
-{
-  {7,7,0},
-  {7,7,0},
-  {0,0,0}
-},
-{
-  {0,7,7},
-  {0,7,7},
-  {0,0,0}
-},
-{
-  {0,0,0},
-  {0,7,7},
-  {0,7,7}
-},
-{  
-  {0,0,0},
-  {7,7,0},
-  {7,7,0}
-}
-};
-
-void initializations(void)
-{
+  		CLKSEL = CLKSEL & 0x80; // disengage PLL from system
+  		PLLCTL = PLLCTL | 0x40; // turn on PLL
+  		SYNR = 0x02;            // set PLL multiplier
+  		REFDV = 0;              // set PLL divider
+  		while (!(CRGFLG & 0x08)){  }
+  		CLKSEL = CLKSEL | 0x80; // engage PLL
   
-  /* Initialize TIM Ch 7 (TC7) for periodic interrupts every 100 ms */
-  TSCR1 = 0x80; // enable timer subsystem
-  TIOS  = 0x80; // set channel 7 for output compare
-  TSCR2 = 0x0C; // set appropriate pre-scale factor and enable counter reset after OC7
-  TC7   = 15000;// set up channel 7 to generate 100 ms interrupt rate
-  TIE   = 0x80; // enable TIM Ch 7 interrupts
-  
-  //initialize gameboard
-  for (y = 0;y < NUMROWS;y++)
-    for (x = 0;x < NUMCOLS;x++) gameboard[y][x] = 0;
-    
-  // other initializations
-  roundover = 1;
-  gameover = 0;
-  level = 0;
-  tick = 0;
-  timcnt = 0;
-  
-}
+// Disable watchdog timer (COPCTL register)
 
-int checkcollision(void)
-{
-  int collision = 0;
-  
-  for (y = 0; y < size; y++)
-  {
-    for (x = 0; x < size; x++)
-    {
-      if (currentpiece[size - 1 - y][x])
-        if ((col + x < 0) || (col + x > 9) || (row + y < 0) || (gameboard[row + y][col + x])) collision = 1;
-    }
-  }
-    
-  return collision;
-}
+      COPCTL = 0x40;    //COP off - RTI and COP stopped in BDM-mode
 
-void clearpiece(void)
-{
-  for (y = 0; y < size; y++)
-    for (x = 0; x < size; x++)
-      if (currentpiece[size - 1 - y][x]) gameboard[row + y][col + x] = 0;
-}
+//  Initialize digital inputs
 
-void setpiece(void)
-{
-  for (y = 0; y < size; y++)
-    for (x = 0; x < size; x++)
-      if (currentpiece[size - 1 - y][x]) gameboard[row + y][col + x] = 1;
-}
+	    DDRAD = 0; 		//program port AD for input mode
+      ATDDIEN = 0x01; //program PAD0 as digital inputs
+         
+//  Add additional port pin initializations here  (e.g., Other DDRs, Ports) 
 
-void flashrows(void)
-{
-  // flashing effect for clear rows
-}
+      DDRT = 0xFF;  // program port T for output mode
+      PTT_PTT0 = 0; // pulse signal
+      PTT_PTT1 = 0; // latch signal
+	  
+//  Add RTI/interrupt initializations here
 
-void clearrows(void)
-{
-  i = 0;
-  row = rclear[i];
-  while (!emptyrow && (i < 4))
-  {
-    emptyrow = 1;
-    while (row == rclear[i])
-    {
-      row++;
-      i++;
-    }
-    for (x = 0; x < size; x++)
-    {
-      gameboard[row-(i+1)][col + x] = gameboard[row][col + x];
-      if (gameboard[row][col + x]) emptyrow = 0;
-    }
-    row++;
-  }
+      CRGINT = 0x80;
+      RTICTL = 0x71;  // approx 60Hz
 }
-
-void updatedisp(void)
-{
-  // probably will become an interrupt - update game board and display
-}
-
+	 		  			 		  		
+ 
+// Main (non-terminating loop)
+ 
 void main(void) {
-  /* put your own code here */
- 	EnableInterrupts;
+	initializations(); 		  			 		  		
+	EnableInterrupts;
+
 
   for(;;) {
 
-  while (!gameover)
-  {
-    if (tick)
-    {
-      tick = 0;
-      
-      if (roundover)
-      {
-        roundover = 0;
-        score += 10 * (level+1);
-        
-        n = (char)(rand() % (NUMPIECES-1)); // set new piece as currentpiece
-        switch (n)
-        {
-          case 0:  block = iblock;
-          break;
-          case 1:  block = tblock;
-          break;
-          case 2:  block = zblock;
-          break;
-          case 3:  block = sblock;
-          break;
-          case 4:  block = jblock;
-          break;
-          case 5:  block = lblock;
-          break;
-          case 6:  block = oblock;
-        }
-        
-        // reset row and col for new piece
-        row = NUMROWS - 4;
-        col = NUMCOLS - 7;
-        
-        if (checkcollision()) gameover = 1;  // unable to initially place new piece, game over
-        else updatedisp();  // update board with initial placement of new piece
-      }
-      else
-      {
-        row--;
-        if (checkcollision())
-        {
-          row++;
-          roundover = 1;
-        }
-        else
-        {
-          row++;
-          clearpiece();
-          row--;
-          setpiece();
-          updatedisp();
-        }
-      }
-    }
-    
-    if (left)
-    {
-      col--;
-      if (checkcollision()) col++;
-      else
-      {
-        col++;
-        clearpiece();
-        col--;
-        setpiece();
-      }
-    }
-    else if (right)
-    {
-      col++;
-      if (checkcollision()) col--;
-      else
-      {
-        col--;
-        clearpiece();
-        col++;
-        setpiece();
-      }
-    }
-    else if (a) // clockwise
-    {
-      rot = (char)(++rot % 4);  //update current piece
-      currentpiece = block[rot];
-      if (checkcollision()) //undo update
-      {
-        rot = (char)(--rot % 4);
-        currentpiece = block[rot];
-      }
-      else
-      {
-        rot = (char)(--rot % 4);
-        currentpiece = block[rot];
-        clearpiece();
-        rot = (char)(++rot % 4);
-        currentpiece = block[rot];
-        setpiece();
-      }
-    }
-    else if (b) // counterclockwise
-    {
-      clearpiece();
-      rot = (char)(--rot % 4);  //update current piece
-      currentpiece = block[rot];
-      if (checkcollision()) //undo update
-      {
-        rot = (char)(++rot % 4);
-        currentpiece = block[rot];
-      }
-      setpiece();
-    }
-    else if (down)
-    {
-      row--;
-      if (checkcollision())
-      {
-        row++;
-        roundover = 1;
-      }
-      else
-      {
-        row++;
-        clearpiece();
-        row--;
-        setpiece();
-      }
-    }
-    else if (up)  // drop piece
-    {
-      clearpiece();
-      do row--;
-      while (!checkcollision());
-      row++;
-      roundover = 1;
-      setpiece();
-    }
-  }
-  
-    _FEED_COP(); /* feeds the dog */
-  }/* loop forever */
-  /* please make sure that you never leave main */
-}
 
-/*
-***********************************************************************
-  TIM interrupt service routine
-***********************************************************************
-*/
+  }/* loop forever */ 
+}/* make sure that you never leave main */
 
-interrupt 15 void TIM_ISR(void)
+
+
+// ***********************************************************************                       
+// RTI interrupt service routine: rti_isr
+// ***********************************************************************                       
+ 
+interrupt 7 void RTI_ISR( void)
 {
- 	TFLG1 = TFLG1 | 0x80; // clear TIM CH 7 interrupt flag 
- 	
- 	timcnt++;
- 	if (timcnt == (10 - level))
- 	{
- 	  timcnt = 0;
- 	  tick = 1;
- 	}
- 	
+  CRGFLG = CRGFLG | 0x80; // set CRGFLG bit to clear RTI device flag
+
+	PTT_PTT1 = 1; // latch data
+	PTT_PTT1 = 0;
+	a = PORTAD0_PTAD0;	// get a
+		
+	PTT_PTT0 = 1; // send pulse
+	PTT_PTT0 = 0;
+	b = PORTAD0_PTAD0;	// get b
+	
+	PTT_PTT0 = 1; // send pulse
+	PTT_PTT0 = 0;
+	sel = PORTAD0_PTAD0;	// get sel
+	
+	PTT_PTT0 = 1; // send pulse
+	PTT_PTT0 = 0;
+	start = PORTAD0_PTAD0;	// get start
+	
+	PTT_PTT0 = 1; // send pulse
+	PTT_PTT0 = 0;
+	up = PORTAD0_PTAD0;	// get up
+	
+	PTT_PTT0 = 1; // send pulse
+	PTT_PTT0 = 0;
+	down = PORTAD0_PTAD0;	// get down
+	
+	PTT_PTT0 = 1; // send pulse
+	PTT_PTT0 = 0;
+	left = PORTAD0_PTAD0;	// get left
+	
+	PTT_PTT0 = 1; // send pulse
+	PTT_PTT0 = 0;
+	right = PORTAD0_PTAD0;	// get right
+		
+	// Output debugging fun
+	if (!b) PTT_PTT7 = 1;
+	else PTT_PTT7 = 0;
+	if (!a) PTT_PTT6 = 1;
+	else PTT_PTT6 = 0;
+
 }
