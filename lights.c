baf9e9abe1548f55b55a6fb0c1f45e3266f3ef65
alabig <alabig@purdue.edu>
2016-04-19 22:10:06 -0400

    Update lights.c

--- lights.c
+++ lights.c
@@ -1,230 +1,564 @@
-/*
-***********************************************************************
- ECE 362 - Final Project - Spring 2016
-***********************************************************************
-*/
-
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
-#include <mc9s12c32.h>
+#include <stdlib.h>
 
-/* All funtions after main should be initialized here */
-void shiftout(char x);
-void lcdwait(void);
+#define TETRIS 4
 
-/* Variable declarations */  	   			 		  			 		       
-char i = 0;
-char j = 0;
+#define NUMROWS 23  // 3 extra rows to allow movement as piece appears
+#define NUMCOLS 10
 
-/* ASCII character definitions */
-#define CR 0x0D	// ASCII return character   
+#define NUMPIECES 7
 
-/* LCD COMMUNICATION BIT MASKS */
-#define RS 0x04		// RS pin mask (PTT[2])
-#define RW 0x08		// R/W pin mask (PTT[3])
-#define LCDCLK 0x10	// LCD EN/CLK pin mask (PTT[4])
+char x, y, size, rot;
+char row, col;
+char emptyrow;
+char *** block;
+char ** currentpiece;
 
-/* LCD INSTRUCTION CHARACTERS */
-#define LCDON 0x0F	// LCD initialization command
-#define LCDCLR 0x01	// LCD clear display command
-#define TWOLINE 0x38	// LCD 2-line enable command
-#define CURMOV 0xFE	// LCD cursor move instruction
-#define LINE1 0x80	// LCD line 1 cursor position
-#define LINE2 0xC0	// LCD line 2 cursor position
+char tick, timcnt, level;
+char roundover, gameover;
 
-/* LED BIT MASKS */
-#define GREEN 0x20
-#define RED 0x40
-#define YELLOW 0x80
+char up, down, left, right, a, b;
 
-/***********************************************************************
-Initializations
-***********************************************************************/
-void  initializations(void) {
+char n, i;
 
-/* Set the PLL speed (bus clock = 24 MHz) */
-  CLKSEL = CLKSEL & 0x80; // disengage PLL from system
-  PLLCTL = PLLCTL | 0x40; // turn on PLL
-  SYNR = 0x02;            // set PLL multiplier
-  REFDV = 0;              // set PLL divider
-  while (!(CRGFLG & 0x08)){  }
-  CLKSEL = CLKSEL | 0x80; // engage PLL
+unsigned char red, green, blue; // RGB values (0-255)
 
-/* Disable watchdog timer (COPCTL register) */
-  COPCTL = 0x40;   //COP off, RTI and COP stopped in BDM-mode
+char rclear[4] = {0,0,0,0};  // rows to clear
 
-/* Initialize SPI for baud rate of 6 Mbs
-  DDRM   = 0xFF;
-  SPICR1 = 0x50;
-  SPICR2 = 0x00;
-  SPIBR  = 0x01;
-*/
+char gameboard[NUMROWS][NUMCOLS];
 
-/* Initialize digital I/O port pins */
-  DDRT = 0xFF; //enable PTT for output
+int score, highscore;
+
+char iblock[4][4][4] = {
+{  
+  {0,0,0,0},
+  {0,0,0,0},
+  {1,1,1,1},
+  {0,0,0,0}
+},
+{
+  {0,1,0,0},
+  {0,1,0,0},
+  {0,1,0,0},
+  {0,1,0,0}
+},
+{
+  {0,0,0,0},
+  {1,1,1,1},
+  {0,0,0,0},
+  {0,0,0,0}
+},
+{
+  {0,0,1,0},
+  {0,0,1,0},
+  {0,0,1,0},
+  {0,0,1,0}
+}
+};
+
+char tblock[4][3][3] = {
+{  
+  {0,2,0},
+  {2,2,2},
+  {0,0,0}
+},
+{
+  {0,2,0},
+  {0,2,2},
+  {0,2,0}
+},
+{
+  {0,0,0},
+  {2,2,2},
+  {0,2,0}
+},
+{
+  {0,2,0},
+  {2,2,0},
+  {0,2,0}
+}
+};
+
+char zblock[4][3][3] = {
+{  
+  {3,3,0},
+  {0,3,3},
+  {0,0,0}
+},
+{
+  {0,3,0},
+  {3,3,0},
+  {3,0,0}
+},
+{
+  {0,0,0},
+  {3,3,0},
+  {0,3,3}
+},
+{
+  {0,0,3},
+  {0,3,3},
+  {0,3,0}
+}
+};
+
+char sblock[4][3][3] = {
+{  
+  {0,4,4},
+  {4,4,0},
+  {0,0,0}
+},
+{
+  {0,4,0},
+  {0,4,4},
+  {0,0,4}
+},
+{
+  {0,0,0},
+  {0,4,4},
+  {4,4,0},
+},
+{
+  {4,0,0},
+  {4,4,0},
+  {0,4,0}
+}
+};
+
+char jblock[4][3][3] = {
+{  
+  {5,0,0},
+  {5,5,5},
+  {0,0,0}
+},
+{
+  {0,5,5},
+  {0,5,0},
+  {0,5,0}
+},
+{
+  {0,0,0},
+  {5,5,5},
+  {0,0,5}
+},
+{
+  {0,5,0},
+  {0,5,0},
+  {5,5,0}
+}
+};
+
+char lblock[4][3][3] = {
+{  
+  {0,0,6},
+  {6,6,6},
+  {0,0,0}
+},
+{
+  {0,6,0},
+  {0,6,0},
+  {0,6,6}
+},
+{
+  {0,0,0},
+  {6,6,6},
+  {6,0,0}
+},
+{
+  {6,6,0},
+  {0,6,0},
+  {0,6,0}
+}
+};
+
+char oblock[4][3][3] = {
+{
+  {7,7,0},
+  {7,7,0},
+  {0,0,0}
+},
+{
+  {0,7,7},
+  {0,7,7},
+  {0,0,0}
+},
+{
+  {0,0,0},
+  {0,7,7},
+  {0,7,7}
+},
+{  
+  {0,0,0},
+  {7,7,0},
+  {7,7,0}
+}
+};
+
+void initializations(void)
+{
+  
+  /* Initialize TIM Ch 7 (TC7) for periodic interrupts every 100 ms */
+  TSCR1 = 0x80; // enable timer subsystem
+  TIOS  = 0x80; // set channel 7 for output compare
+  TSCR2 = 0x0C; // set appropriate pre-scale factor and enable counter reset after OC7
+  TC7   = 15000;// set up channel 7 to generate 100 ms interrupt rate
+  TIE   = 0x80; // enable TIM Ch 7 interrupts
+  
+  //initialize gameboard
+  for (y = 0;y < NUMROWS;y++)
+    for (x = 0;x < NUMCOLS;x++) gameboard[y][x] = 0;
+    
+  // other initializations
+  roundover = 1;
+  gameover = 0;
+  level = 0;
+  tick = 0;
+  timcnt = 0;
+  
+}
+
+int checkcollision(void)
+{
+  int collision = 0;
+  
+  for (y = 0; y < size; y++)
+  {
+    for (x = 0; x < size; x++)
+    {
+      if (currentpiece[size - 1 - y][x])
+        if ((col + x < 0) || (col + x > 9) || (row + y < 0) || (gameboard[row + y][col + x])) collision = 1;
+    }
+  }
+    
+  return collision;
+}
+
+void clearpiece(void)
+{
+  for (y = 0; y < size; y++)
+    for (x = 0; x < size; x++)
+      if (currentpiece[size - 1 - y][x]) gameboard[row + y][col + x] = 0;
+}
+
+void setpiece(void)
+{
+  for (y = 0; y < size; y++)
+    for (x = 0; x < size; x++)
+      if (currentpiece[size - 1 - y][x]) gameboard[row + y][col + x] = 1;
+}
+
+void flashrows(void)
+{
+  // flashing effect for clear rows
+}
+
+void clearrows(void)
+{
+  i = 0;
+  row = rclear[i];
+  while (!emptyrow && (i < 4))
+  {
+    emptyrow = 1;
+    while (row == rclear[i])
+    {
+      row++;
+      i++;
+    }
+    for (x = 0; x < size; x++)
+    {
+      gameboard[row-(i+1)][col + x] = gameboard[row][col + x];
+      if (gameboard[row][col + x]) emptyrow = 0;
+    }
+    row++;
+  }
+}
+
+void clockdisp(void)
+{
+  PTT_PTT0 = 1;
   PTT_PTT0 = 0;
-  PTT_PTT1 = 0;
-
-/* Initialize the LCD */  
-//  lcdwait();      // wait for 2ms so that the LCD can wake up   
-  
 }
-	 		  			 		  		
-/***********************************************************************
-Main
-***********************************************************************/
-void main(void) {
-  DisableInterrupts;
-	initializations(); 		  			 		  		
-	EnableInterrupts;
 
-  //for(;;) {
-  
-  /* write your code here */
-  
-  /*
-  lcdwait();            // wait for (at least <>) 30 cycles for SPI data to shift out  
+void updatedisp(void) // update LED data
+{
   // start frame
-  shiftout(0x00);
-  shiftout(0x00);
-  shiftout(0x00);
-  shiftout(0x00);
-  
-  // LED frame
-  shiftout(0xFF);  // brightness
-  shiftout(0xFF);  // blue
-  shiftout(0x00);  // green
-  shiftout(0x00);  // red
-    
-  for (i=0;i<29;i++)  // blank frames
-  {
-    shiftout(0xE0);
-    shiftout(0x00);
-    shiftout(0x00);
-    shiftout(0x00);
-  }
-    
-  // end frame for 60 LEDs
-  shiftout(0x00);
-  shiftout(0x00);
-  //shiftout(0x00);
-  //shiftout(0x00);
-  */
-  
-  PTT_PTT1 = 0; // start frame
+  PTT_PTT1 = 0;
   for (i=0;i<32;i++)
-  {
-    PTT_PTT0 = 1;
-    PTT_PTT0 = 0;
-  }
+    clockdisp();
   
-  for (j=0;j<30;j++)
-  {
-    PTT_PTT1 = 1; //  brightness
-    for (i=0;i<8;i++)
+  for (x = 0;x < NUMCOLS; x++)
+    for (y = 0;y < (NUMROWS - 3); y++)
     {
-      PTT_PTT0 = 1;
-      PTT_PTT0 = 0;
+      switch (gameboard[y][x])
+      {
+        case 0: red = 0;    // blank
+                green = 0;
+                blue = 0;
+                break;
+        case 1: red = 0;    // cyan (i-block)
+                green = 240;
+                blue = 240;
+                break;
+        case 2: red = 161;  // purple (t-block)
+                green = 0;
+                blue = 240;
+                break;
+        case 3: red = 255;  // red (z-block)
+                green = 0;
+                blue = 0;
+                break;
+        case 4: red = 0;    // green (s-block)
+                green = 255;
+                blue = 0;
+                break;
+        case 5: red = 0;    // blue (j-block)
+                green = 0;
+                blue = 255;
+                break;
+        case 6: red = 240;  // orange (l-block)
+                green = 161;
+                blue = 0;
+                break;
+        case 7: red = 240;  // yellow (o-block)
+                green = 240;
+                blue = 0;
+                break;
+        default:red = 255;  // white by default (it's not a racial thing)
+                green = 255;
+                blue = 255;
+      }
+      
+      // header frame (brightness)
+      PTT_PTT1 = 1;
+      for (i=0;i<8;i++)
+        clockdisp();
+      
+      //LED frame
+      PTT_PTT1 = blue & %00000001;
+      clockdisp();
+      PTT_PTT1 = blue & %00000010;
+      clockdisp();
+      PTT_PTT1 = blue & %00000100;
+      clockdisp();
+      PTT_PTT1 = blue & %00001000;
+      clockdisp();
+      PTT_PTT1 = blue & %00010000;
+      clockdisp();
+      PTT_PTT1 = blue & %00100000;
+      clockdisp();
+      PTT_PTT1 = blue & %01000000;
+      clockdisp();
+      PTT_PTT1 = blue & %10000000;
+      clockdisp();
+      
+      PTT_PTT1 = green & %00000001;
+      clockdisp();
+      PTT_PTT1 = green & %00000010;
+      clockdisp();
+      PTT_PTT1 = green & %00000100;
+      clockdisp();
+      PTT_PTT1 = green & %00001000;
+      clockdisp();
+      PTT_PTT1 = green & %00010000;
+      clockdisp();
+      PTT_PTT1 = green & %00100000;
+      clockdisp();
+      PTT_PTT1 = green & %01000000;
+      clockdisp();
+      PTT_PTT1 = green & %10000000;
+      clockdisp();
+      
+      PTT_PTT1 = red & %00000001;
+      clockdisp();
+      PTT_PTT1 = red & %00000010;
+      clockdisp();
+      PTT_PTT1 = red & %00000100;
+      clockdisp();
+      PTT_PTT1 = red & %00001000;
+      clockdisp();
+      PTT_PTT1 = red & %00010000;
+      clockdisp();
+      PTT_PTT1 = red & %00100000;
+      clockdisp();
+      PTT_PTT1 = red & %01000000;
+      clockdisp();
+      PTT_PTT1 = red & %10000000;
+      clockdisp();
+      
     }
     
+    // end frame for 200 LEDs
     PTT_PTT1 = 0;
-    for (i=0;i<8;i++) // blue
-    {
-      PTT_PTT0 = 1;
-      PTT_PTT0 = 0;
-    }
-    
-    PTT_PTT1 = 0;
-    for (i=0;i<8;i++) // green
-    {
-      PTT_PTT0 = 1;
-      PTT_PTT0 = 0;
-    }
-    
-    PTT_PTT1 = 1;
-    for (i=0;i<8;i++) // red
-    {
-      PTT_PTT0 = 1;
-      PTT_PTT0 = 0;
-    }
+    for (j=0;j<13;j++)
+      for (i=0;i<8;i++)
+        clockdisp();
 
-    PTT_PTT1 = 1; //  brightness
-    for (i=0;i<8;i++)
-    {
-      PTT_PTT0 = 1;
-      PTT_PTT0 = 0;
-    }
-    
-    PTT_PTT1 = 1;
-    for (i=0;i<8;i++) // blue
-    {
-      PTT_PTT0 = 1;
-      PTT_PTT0 = 0;
-    }
-    
-    PTT_PTT1 = 0;
-    for (i=0;i<8;i++) // green
-    {
-      PTT_PTT0 = 1;
-      PTT_PTT0 = 0;
-    }
-    
-    PTT_PTT1 = 0;
-    for (i=0;i<8;i++) // red
-    {
-      PTT_PTT0 = 1;
-      PTT_PTT0 = 0;
-    }
-  }
-  
-  PTT_PTT1 = 0; // end frame
-  for (j=0;j<4;j++)
-  {
-    for (i=0;i<8;i++)
-    {
-      PTT_PTT0 = 1;
-      PTT_PTT0 = 0;
-    }
-  }
-    
-  //}/* loop forever */
-}/* do not leave main */
-
-
-/***********************************************************************
-  shiftout: Transmits the character x to external shift 
-            register using the SPI.  It should shift MSB first.  
-             
-            MOSI = PM[4]
-            SCK  = PM[5]
-***********************************************************************/
-
-void shiftout(char x)
-
-{
-  while(!SPISR_SPTEF) {}// read the SPTEF bit, continue if bit is 1
-  SPIDR = x;            // write data to SPI data register
-  //lcdwait();            // wait for (at least <>) 30 cycles for SPI data to shift out  
-  
 }
 
+void main(void) {
+  /* put your own code here */
+ 	EnableInterrupts;
 
-/***********************************************************************
-  lcdwait: Delay for approx 2 ms
-***********************************************************************/
-void lcdwait()
-{
-  int m = 5;
-  int n = 5000;
-  while(m > 0)
+  for(;;) {
+
+  while (!gameover)
   {
-    while(n > 0)
+    if (tick)
     {
-      n--;
-      asm{
-      nop;
+      tick = 0;
+      
+      if (roundover)
+      {
+        roundover = 0;
+        score += 10 * (level+1);
+        
+        n = (char)(rand() % (NUMPIECES-1)); // set new piece as currentpiece
+        switch (n)
+        {
+          case 0:  block = iblock;
+          break;
+          case 1:  block = tblock;
+          break;
+          case 2:  block = zblock;
+          break;
+          case 3:  block = sblock;
+          break;
+          case 4:  block = jblock;
+          break;
+          case 5:  block = lblock;
+          break;
+          case 6:  block = oblock;
+        }
+        
+        // reset row and col for new piece
+        row = NUMROWS - 4;
+        col = NUMCOLS - 7;
+        
+        if (checkcollision()) gameover = 1;  // unable to initially place new piece, game over
+        else updatedisp();  // update board with initial placement of new piece
+      }
+      else
+      {
+        row--;
+        if (checkcollision())
+        {
+          row++;
+          roundover = 1;
+        }
+        else
+        {
+          row++;
+          clearpiece();
+          row--;
+          setpiece();
+          updatedisp();
+        }
       }
     }
-    m--;
+    
+    if (left)
+    {
+      col--;
+      if (checkcollision()) col++;
+      else
+      {
+        col++;
+        clearpiece();
+        col--;
+        setpiece();
+      }
+    }
+    else if (right)
+    {
+      col++;
+      if (checkcollision()) col--;
+      else
+      {
+        col--;
+        clearpiece();
+        col++;
+        setpiece();
+      }
+    }
+    else if (a) // clockwise
+    {
+      rot = (char)(++rot % 4);  //update current piece
+      currentpiece = block[rot];
+      if (checkcollision()) //undo update
+      {
+        rot = (char)(--rot % 4);
+        currentpiece = block[rot];
+      }
+      else
+      {
+        rot = (char)(--rot % 4);
+        currentpiece = block[rot];
+        clearpiece();
+        rot = (char)(++rot % 4);
+        currentpiece = block[rot];
+        setpiece();
+      }
+    }
+    else if (b) // counterclockwise
+    {
+      clearpiece();
+      rot = (char)(--rot % 4);  //update current piece
+      currentpiece = block[rot];
+      if (checkcollision()) //undo update
+      {
+        rot = (char)(++rot % 4);
+        currentpiece = block[rot];
+      }
+      setpiece();
+    }
+    else if (down)
+    {
+      row--;
+      if (checkcollision())
+      {
+        row++;
+        roundover = 1;
+      }
+      else
+      {
+        row++;
+        clearpiece();
+        row--;
+        setpiece();
+      }
+    }
+    else if (up)  // drop piece
+    {
+      clearpiece();
+      do row--;
+      while (!checkcollision());
+      row++;
+      roundover = 1;
+      setpiece();
+    }
   }
+  
+    _FEED_COP(); /* feeds the dog */
+  }/* loop forever */
+  /* please make sure that you never leave main */
+}
+
+/*
+***********************************************************************
+  TIM interrupt service routine
+***********************************************************************
+*/
+
+interrupt 15 void TIM_ISR(void)
+{
+ 	TFLG1 = TFLG1 | 0x80; // clear TIM CH 7 interrupt flag 
+ 	
+ 	timcnt++;
+ 	if (timcnt == (10 - level))
+ 	{
+ 	  timcnt = 0;
+ 	  tick = 1;
+ 	}
+ 	
 }
