7b054f92a203a56320c17004a128e78a241f9b39
alabig <alabig@purdue.edu>
2016-04-19 22:11:39 -0400

    Update tetris.c

--- tetris.c
+++ tetris.c
@@ -11,17 +11,24 @@
 
 char x, y, size, rot;
 char row, col;
+char emptyrow;
 char *** block;
 char ** currentpiece;
 
 char tick, timcnt, level;
 char roundover, gameover;
 
-char up, down, left, right, clockwise, cclockwise;
+char up, down, left, right, a, b;
 
-char n;
+char n, i;
+
+unsigned char red, green, blue; // RGB values (0-255)
+
+char rclear[4] = {0,0,0,0};  // rows to clear
 
 char gameboard[NUMROWS][NUMCOLS];
+
+int score, highscore;
 
 char iblock[4][4][4] = {
 {  
@@ -241,9 +248,152 @@ void setpiece(void)
       if (currentpiece[size - 1 - y][x]) gameboard[row + y][col + x] = 1;
 }
 
-void updatedisp(void)
+void flashrows(void)
 {
-  // probably will become an interrupt - update game board and display
+  // flashing effect for clear rows
+}
+
+void clearrows(void)
+{
+  i = 0;
+  row = rclear[i];
+  while (!emptyrow && (i < 4))
+  {
+    emptyrow = 1;
+    while (row == rclear[i])
+    {
+      row++;
+      i++;
+    }
+    for (x = 0; x < size; x++)
+    {
+      gameboard[row-(i+1)][col + x] = gameboard[row][col + x];
+      if (gameboard[row][col + x]) emptyrow = 0;
+    }
+    row++;
+  }
+}
+
+void clockdisp(void)
+{
+  PTT_PTT0 = 1;
+  PTT_PTT0 = 0;
+}
+
+void updatedisp(void) // update LED data
+{
+  // start frame
+  PTT_PTT1 = 0;
+  for (i=0;i<32;i++)
+    clockdisp();
+  
+  for (x = 0;x < NUMCOLS; x++)
+    for (y = 0;y < (NUMROWS - 3); y++)
+    {
+      switch (gameboard[y][x])
+      {
+        case 0: red = 0;    // blank
+                green = 0;
+                blue = 0;
+                break;
+        case 1: red = 0;    // cyan (i-block)
+                green = 240;
+                blue = 240;
+                break;
+        case 2: red = 161;  // purple (t-block)
+                green = 0;
+                blue = 240;
+                break;
+        case 3: red = 255;  // red (z-block)
+                green = 0;
+                blue = 0;
+                break;
+        case 4: red = 0;    // green (s-block)
+                green = 255;
+                blue = 0;
+                break;
+        case 5: red = 0;    // blue (j-block)
+                green = 0;
+                blue = 255;
+                break;
+        case 6: red = 240;  // orange (l-block)
+                green = 161;
+                blue = 0;
+                break;
+        case 7: red = 240;  // yellow (o-block)
+                green = 240;
+                blue = 0;
+                break;
+        default:red = 255;  // white by default (it's not a racial thing)
+                green = 255;
+                blue = 255;
+      }
+      
+      // header frame (brightness)
+      PTT_PTT1 = 1;
+      for (i=0;i<8;i++)
+        clockdisp();
+      
+      //LED frame
+      PTT_PTT1 = blue & %00000001;
+      clockdisp();
+      PTT_PTT1 = blue & %00000010;
+      clockdisp();
+      PTT_PTT1 = blue & %00000100;
+      clockdisp();
+      PTT_PTT1 = blue & %00001000;
+      clockdisp();
+      PTT_PTT1 = blue & %00010000;
+      clockdisp();
+      PTT_PTT1 = blue & %00100000;
+      clockdisp();
+      PTT_PTT1 = blue & %01000000;
+      clockdisp();
+      PTT_PTT1 = blue & %10000000;
+      clockdisp();
+      
+      PTT_PTT1 = green & %00000001;
+      clockdisp();
+      PTT_PTT1 = green & %00000010;
+      clockdisp();
+      PTT_PTT1 = green & %00000100;
+      clockdisp();
+      PTT_PTT1 = green & %00001000;
+      clockdisp();
+      PTT_PTT1 = green & %00010000;
+      clockdisp();
+      PTT_PTT1 = green & %00100000;
+      clockdisp();
+      PTT_PTT1 = green & %01000000;
+      clockdisp();
+      PTT_PTT1 = green & %10000000;
+      clockdisp();
+      
+      PTT_PTT1 = red & %00000001;
+      clockdisp();
+      PTT_PTT1 = red & %00000010;
+      clockdisp();
+      PTT_PTT1 = red & %00000100;
+      clockdisp();
+      PTT_PTT1 = red & %00001000;
+      clockdisp();
+      PTT_PTT1 = red & %00010000;
+      clockdisp();
+      PTT_PTT1 = red & %00100000;
+      clockdisp();
+      PTT_PTT1 = red & %01000000;
+      clockdisp();
+      PTT_PTT1 = red & %10000000;
+      clockdisp();
+      
+    }
+    
+    // end frame for 200 LEDs
+    PTT_PTT1 = 0;
+    for (j=0;j<13;j++)
+      for (i=0;i<8;i++)
+        clockdisp();
+
 }
 
 void main(void) {
@@ -261,6 +411,7 @@ void main(void) {
       if (roundover)
       {
         roundover = 0;
+        score += 10 * (level+1);
         
         n = (char)(rand() % (NUMPIECES-1)); // set new piece as currentpiece
         switch (n)
@@ -330,13 +481,13 @@ void main(void) {
         setpiece();
       }
     }
-    else if (clockwise)
+    else if (a) // clockwise
     {
       rot = (char)(++rot % 4);  //update current piece
       currentpiece = block[rot];
       if (checkcollision()) //undo update
       {
-        (char)(rot = --rot % 4);
+        rot = (char)(--rot % 4);
         currentpiece = block[rot];
       }
       else
@@ -349,7 +500,7 @@ void main(void) {
         setpiece();
       }
     }
-    else if (cclockwise)
+    else if (b) // counterclockwise
     {
       clearpiece();
       rot = (char)(--rot % 4);  //update current piece
